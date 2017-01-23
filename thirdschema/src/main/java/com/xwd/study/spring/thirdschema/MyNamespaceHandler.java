552104a24dd8232c43345603089ee2cd0b2e873d
x <xwd@xiaowudong>
2017-01-23 12:19:42 +0800

    新增测试模块

---
+++ thirdschema/src/main/java/com/xwd/study/spring/thirdschema/MyNamespaceHandler.java
@@ -0,0 +1,12 @@
+package com.xwd.study.spring.thirdschema;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+
+	@Override
+	public void init() {
+		registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());  // TODO Auto-generated method stub
+	}
+
+}
